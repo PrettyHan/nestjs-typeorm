b2d9a73c78a41a52b269b9241656cd1f4d4bf55b
PrettyHan <imbel9830@gmail.com>
2022-08-09 21:38:34 +0900

    add : typeorm entity 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,33 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { CatsModule } from './cats/cats.module';
+import { LoggerMiddleware } from './cats/middleware/logger.middleware';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Cat } from './cats/entity/cats.entity';
 
 @Module({
-  imports: [UsersModule, CatsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD,
+      database: 'test',
+      entities: [Cat],
+      synchronize: true,
+    }),
+    UsersModule,
+    CatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('cats');
+    // cats~ 에 대한 미들웨어 적용
+  }
+}
--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { CatsController } from './cats.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Cat } from './entity/cats.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Cat])],
+  exports: [TypeOrmModule],
   providers: [CatsService],
-  controllers: [CatsController]
+  controllers: [CatsController],
 })
 export class CatsModule {}
